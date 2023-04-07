488f74efff947dde9444b811727da4339233c234
shrinikam <shrirang348@gmail.com>
2023-04-07 12:48:27 +0530

    FirstCompApp

---
+++ src/app/first-comp/first-comp.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-first-comp',
+  template: '<p>Hello from First component</p>'
+})
+export class FirstCompComponent {
+
+}
